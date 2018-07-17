a45074428398218d69d26ca182c4008bd9cd76c0
wangjie <13533801264@163.com>
2018-07-17 17:44:32 +0800

    fix some bugs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-def post_list(requst):
-    return render(requst,'blog/post_list.html',{})
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
 
